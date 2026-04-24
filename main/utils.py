9694edab92c837ac1829ac637f9705887441ed58
Sairendri02 <swainsairendri@gmail.com>
2026-04-24 13:24:04 +0530

    Remove credentials from code

--- main/utils.py
+++ main/utils.py
@@ -1,11 +1,13 @@
 import os
 from dotenv import load_dotenv
 from twilio.rest import Client
+
 load_dotenv()
+
 def send_otp_sms(phone, otp):
-    account_sid = "AC57db65901e98a7a5fc6f2c49a9dc15cf"
-    auth_token = "538a357e17a9cef101811732027802bc"
-    twilio_phone = "+12295754614"
+    account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+    auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+    twilio_phone = os.environ.get("TWILIO_PHONE")
 
     client = Client(account_sid, auth_token)
     try:
